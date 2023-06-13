5c9029ce608eeab3f80fee2d8357ffe51711d759
Edgar Rosales <erosales1261@gmail.com>
2023-06-12 23:53:35 -0400

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,11 @@ windows.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getresumecounterz.azurewebsites.net/api/GetResumeCounter?code=G-DuGEgdT4w3I98dSO2Cni7HWwoeXs0AMDX5soG3WHR4AzFu-Xuawg==';
-const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://getresumecounterz.azurewebsites.net/api/GetResumeCounter?code=G-DuGEgdT4w3I98dSO2Cni7HWwoeXs0AMDX5soG3WHR4AzFu-Xuawg==';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
+    fetch(functionApi).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
